4289e2919f1f4a9f234433187e4de9a932db98fc
Jörgen Abrahamsson <jorgen@ribbingsfors.se>
2020-05-28 14:28:38 +0200

    Scaffolded Identity item. Added Dbset property GymClasses to ApplicationDbContext class.

--- Booking2/Data/ApplicationDbContext.cs
+++ Booking2/Data/ApplicationDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Booking2.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +9,7 @@ namespace Booking2.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<GymClass> GymClasses { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
